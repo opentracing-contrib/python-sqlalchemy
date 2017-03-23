bb139e3aab3ddc268981af43445430c7123b468a
Carlos Alberto Cortez <calberto.cortez@gmail.com>
2017-03-23 20:32:05 +0100

    Implement initial support for ORM tracing.
    This is done for each Session's transaction
    lifetime, which means a Session has to be registered
    (and optionally set with a parent span).

---
+++ examples/orm-childspan.py
@@ -0,0 +1,54 @@
+import os
+from sqlalchemy import MetaData, Table, Integer, String, Column, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+import lightstep
+import sqlalchemy_opentracing
+
+DB_LOCATION = '/tmp/simple.db'
+
+tracer = lightstep.Tracer(
+    component_name='sqlalchemy-orm-childspan',
+    access_token='{your_lightstep_token}'
+)
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+
+if __name__ == '__main__':
+    if os.path.exists(DB_LOCATION):
+        os.remove(DB_LOCATION) # cleanup
+
+    engine = create_engine('sqlite:///%s' % DB_LOCATION)
+    session = sessionmaker(bind=engine)()
+
+    sqlalchemy_opentracing.init_tracing(tracer)
+    sqlalchemy_opentracing.register_connectable(engine)
+
+    User.metadata.create_all(engine)
+
+    # Create a parent span
+    span = tracer.start_span('parent span')
+
+    # Register the session for the current transaction.
+    sqlalchemy_opentracing.set_parent_span(session, span)
+
+    session.add(User(name='John Doe'))
+    session.add(User(name='Jason Bourne'))
+
+    users = session.query(User).all()
+    for user in users:
+        print user.name
+
+    session.add(User(name='Who'))
+
+    # Commit the session and close the parent span.
+    session.commit()
+    span.finish()
+
---
+++ examples/orm-simple.py
@@ -0,0 +1,45 @@
+import os
+from sqlalchemy import MetaData, Table, Integer, String, Column, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+import lightstep
+import sqlalchemy_opentracing
+
+DB_LOCATION = '/tmp/simple.db'
+
+tracer = lightstep.Tracer(
+    component_name='sqlalchemy-orm-simple',
+    access_token='{your_lightstep_token}'
+)
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+
+if __name__ == '__main__':
+    if os.path.exists(DB_LOCATION):
+        os.remove(DB_LOCATION) # cleanup
+
+    engine = create_engine('sqlite:///%s' % DB_LOCATION)
+    session = sessionmaker(bind=engine)()
+
+    sqlalchemy_opentracing.init_tracing(tracer)
+    sqlalchemy_opentracing.register_connectable(engine)
+
+    User.metadata.create_all(engine)
+
+    # Register the session for the current transaction.
+    sqlalchemy_opentracing.set_traced(session)
+
+    # Insert a new row.
+    session.add(User(name='John Doe'))
+
+    # Finish the commit.
+    # This will stop tracing the session.
+    session.commit()
+
